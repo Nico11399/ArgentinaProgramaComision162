095478a84ca42ef8266c59c7660c0868cb22108b
Nico11399 <nico11399@gmail.com>
2023-03-20 11:32:04 -0300

    creando mi primer repositorio

---
+++ script.js
@@ -0,0 +1,37 @@
+/* for(let i = 1; i <= 10; i = i + 1) {
+    if(i % 2 == 0){
+        console.log(i + " es par")
+    }
+    
+} */
+
+/* 
+const puedeVerPelicula = (edad, tieneAutorizacion) => {
+    return edad >= 15 || tieneAutorizacion
+
+} */
+
+/* let estaSoleado = false
+let tengoPlata = false
+
+if(estaSoleado || tengoPlata){
+    console.log("voy al parque")
+} */
+let contador = 0
+
+contador++
+contador++
+contador++
+contador++
+contador++
+contador++
+contador++
+contador++
+contador++
+contador++
+contador++
+contador++
+contador++
+contador++
+contador++
+contador++
